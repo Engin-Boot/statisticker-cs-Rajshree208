98fb7af7b428ca9ecefefc6eda82cbd9760601fd
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-11 10:31:43 +0530

    Update Statistics.cs

--- Statistics/Statistics.cs
+++ Statistics/Statistics.cs
@@ -3,10 +3,52 @@ using System.Collections.Generic;
 
 namespace Statistics
 {
+    class Stats
+    {
+        public double average;
+        public double min;
+        public double max;
+        public Stats(double average, double min, double max)
+        {
+            this.average = average;
+            this.min = min;
+            this.max = max;
+        }
+    }
+    
     public class StatsComputer
     {
-        public Stats CalculateStatistics(List<float> numbers) {
-            //Implement statistics here
+        double getAverage(List<double> numbers)
+        {
+            if (numbers.Count == 0)
+                return Double.NaN;
+            else
+                return (numbers.AsQueryable().Sum() / numbers.Count);
+        }
+
+        double getMin(List<double> numbers)
+        {
+            numbers.Sort();
+            if (numbers.Count != 0)
+            {
+                return numbers[0];
+            }
+            return Double.NaN;
+        }
+
+        double getMax(List<double> numbers)
+        {
+            numbers.Sort();
+            if (numbers.Count!=0)
+            {
+                return numbers[numbers.Count- 1];
+            }
+            return Double.NaN;
+        }
+        
+        public Stats CalculateStatistics(List<double> numbers) {
+            Stats stats = new Stats(getAverage(numbers), getMin(numbers), getMax(numbers));
+            return stats;
         }
     }
 }
