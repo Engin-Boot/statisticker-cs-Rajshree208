09b25c9a94612700bdb2c9a13ef108f8d718f468
Rajshree208 <46247383+Rajshree208@users.noreply.github.com>
2020-09-11 10:51:46 +0530

    Update StatsUnitTest.cs

--- Statistics.Test/StatsUnitTest.cs
+++ Statistics.Test/StatsUnitTest.cs
@@ -1,6 +1,7 @@
 using System;
 using Xunit;
 using Statistics;
+using System.Collections.Generic;
 
 namespace Statistics.Test
 {
@@ -13,9 +14,9 @@ namespace Statistics.Test
             var computedStats = statsComputer.CalculateStatistics(
                 new List<double>{1.5, 8.9, 3.2, 4.5});
             float epsilon = 0.001F;
-            Assert.True(Math.Abs(statsComputer.average - 4.525) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.max - 8.9) <= epsilon);
-            Assert.True(Math.Abs(statsComputer.min - 1.5) <= epsilon);
+            Assert.True(Math.Abs(computedStats.average - 4.525) <= epsilon);
+            Assert.True(Math.Abs(computedStats.max - 8.9) <= epsilon);
+            Assert.True(Math.Abs(computedStats.min - 1.5) <= epsilon);
         }
         public void ReportsNaNForEmptyInput()
         {
@@ -25,9 +26,9 @@ namespace Statistics.Test
             //All fields of computedStats (average, max, min) must be
             //Double.NaN (not-a-number), as described in
             //https://docs.microsoft.com/en-us/dotnet/api/system.double.nan?view=netcore-3.1
-            Assert.True(Double.IsNaN(statsComputer.average)==true);
-            Assert.True(Double.IsNaN(statsComputer.max) == true);
-            Assert.True(Double.IsNaN(statsComputer.min) == true);
+            Assert.True(Double.IsNaN(computedStats.average)==true);
+            Assert.True(Double.IsNaN(computedStats.max) == true);
+            Assert.True(Double.IsNaN(computedStats.min) == true);
         }
     }
 }
